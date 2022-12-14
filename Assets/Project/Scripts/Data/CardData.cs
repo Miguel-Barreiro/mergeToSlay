cae30ea1ebc263de7b01989b308be0997cced6e8
Miguel Barreiro <miguelb@gram.gs>
2022-12-14 12:47:28 +0000

    added enemy data

--- Assets/Project/Scripts/Data/CardData.cs
+++ Assets/Project/Scripts/Data/CardData.cs
@@ -9,7 +9,7 @@ namespace MergeToStay.Data
     [CreateAssetMenu(fileName = "NEW_CARD", menuName = "MergeToSlay.CARD/new CARD", order = 0)]
     public sealed class CardData : ScriptableObject
     {
-        
+        public CardsModel.CardRarity CardRarity = CardsModel.CardRarity.Common;
         public string Name;
 
         [InfoBox("This is where we define each level data for the card")]
---
+++ Assets/Project/Scripts/Data/EnemyData.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using MergeToStay.Data.Actions;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MergeToStay.Data
+{
+	[CreateAssetMenu(fileName = "NEW_ENEMY", menuName = "MergeToSlay.COMBAT/new ENEMY", order = 0)]
+	public sealed class EnemyData : ScriptableObject
+	{
+		
+		[Range(1, 200)] 
+		public int Hp = 1;
+		
+		public string Name;
+		
+		public GameObject Prefab;
+		
+		public bool IsBoss = false;
+		
+		[SerializeField]
+		public List<CombatSequence> CombatBehaviours;
+	}
+
+	[Serializable]
+	public sealed class CombatSequence
+	{
+		[SerializeField] 
+		public List<TurnActions> TurnActionsSequence;
+	}
+	
+	[Serializable]
+	public sealed class TurnActions
+	{
+		[SerializeField]
+		public List<TurnAction> Actions;
+	}
+	
+	[Serializable]
+	public sealed class TurnAction
+	{
+		public GameObject Icon;
+		public ActionBase Action;
+	}
+}
