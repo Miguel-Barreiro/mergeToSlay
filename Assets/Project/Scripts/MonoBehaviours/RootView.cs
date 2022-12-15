fca14ea03acb7b06668e16a2cdb6b8f0bb2811a0
Jefferson Rodrigo Carvalho Gouveia <jeffersonrcgouveia@gmail.com>
2022-12-15 14:05:08 +0000

    Added the RootView with the Path root to the Canvas prefab to avoid conflicts with other feature

--- Assets/Project/Scripts/MonoBehaviours/RootView.cs
+++ Assets/Project/Scripts/MonoBehaviours/RootView.cs
@@ -4,9 +4,8 @@ namespace MergeToStay.MonoBehaviours
 {
 	public class RootView : MonoBehaviour
 	{
-
 		public RectTransform BoardRoot;
 		public  RectTransform GridObjectsRoot;
-
+		public  RectTransform PathRoot;
 	}
 }
