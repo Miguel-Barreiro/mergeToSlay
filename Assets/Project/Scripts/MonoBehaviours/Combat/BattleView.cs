5949d51ca3475e78361c198fcbd5aee7312c98ae
Miguel Barreiro <miguelb@gram.gs>
2022-12-16 22:18:22 +0000

    added a way to see the player defense

--- Assets/Project/Scripts/MonoBehaviours/Combat/BattleView.cs
+++ Assets/Project/Scripts/MonoBehaviours/Combat/BattleView.cs
@@ -1,14 +1,45 @@
 using System.Collections.Generic;
+using Entitas;
+using TMPro;
 using UnityEngine;
+using Zenject;
 
 namespace MergeToStay.MonoBehaviours.Combat
 {
     public class BattleView : MonoBehaviour
     {
+        [Inject] protected Contexts _contexts;
+        
+        public TMP_Text PlayerDefense;
         public List<EnemyStatusView> EnemyStatusViews;
         public List<RectTransform> EnemySpots;
         public RectTransform PlayerSpot;
 
+        private IGroup<GameEntity> _batleGroup;
+
+        private void Start()
+        {
+            _batleGroup = _contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.Battle));
+        }
+
+        private void Update()
+        {
+            GameEntity battleEntity = _batleGroup.GetSingleEntity();
+            if (battleEntity == null)
+                return;
+
+            int defense = battleEntity.battle.PlayerCurrentTurnStats.Defense;
+
+            if (defense > 0)
+            {
+                PlayerDefense.gameObject.SetActive(true);
+                PlayerDefense.text = defense.ToString();
+            } else
+            {
+                PlayerDefense.gameObject.SetActive(false);
+                PlayerDefense.text = defense.ToString();
+            }
+        }
     }
     
 }
