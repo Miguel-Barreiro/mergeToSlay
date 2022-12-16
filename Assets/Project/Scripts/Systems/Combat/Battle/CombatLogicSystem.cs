1abc59e4009336d5267dcb93aaa45a083296a1ee
Miguel Barreiro <miguelb@gram.gs>
2022-12-16 10:46:05 +0000

    fix?

--- Assets/Project/Scripts/Systems/Combat/Battle/CombatLogicSystem.cs
+++ Assets/Project/Scripts/Systems/Combat/Battle/CombatLogicSystem.cs
@@ -5,7 +5,28 @@ namespace MergeToStay.Systems.Combat.Battle
 {
 	public class CombatLogicSystem : ReactiveGameSystem
 	{
-		protected override void Execute(List<GameEntity> entities) {  }
+		
+		private IGroup<GameEntity> _batleGroup;
+
+		public void Initialize()
+		{
+			_batleGroup = _contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.Battle));
+		}
+
+		protected override void Execute(List<GameEntity> entities)
+		{
+			GameEntity battleEntity = _batleGroup.GetSingleEntity();
+
+
+			foreach (GameEntity eventEntity in entities)
+			{
+				
+				
+				eventEntity.Destroy();
+			}
+			
+			
+		}
 
 		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
 		{
