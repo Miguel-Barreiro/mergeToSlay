e8134a47dcd18e4aa697052a0521b8bb2577d8d1
Miguel Barreiro <miguelb@gram.gs>
2022-12-16 12:22:34 +0000

    game state flow works

--- Assets/Project/Scripts/Systems/Combat/Battle/CombatLogicSystem.cs
+++ Assets/Project/Scripts/Systems/Combat/Battle/CombatLogicSystem.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Entitas;
 using MergeToStay.Components.Combat;
 using MergeToStay.MonoBehaviours.Combat;
+using MergeToStay.Services;
 using Zenject;
 
 namespace MergeToStay.Systems.Combat.Battle
@@ -9,7 +10,10 @@ namespace MergeToStay.Systems.Combat.Battle
 	public class CombatLogicSystem : ReactiveGameSystem, IInitializeSystem
 	{
 
+		[Inject] private GameContext _context;
 		[Inject] private BoardView _boardView;
+		[Inject] private CombatService _combatService;
+		[Inject] private BoardService _boardService;
 		
 		private IGroup<GameEntity> _batleGroup;
 
@@ -61,11 +65,44 @@ namespace MergeToStay.Systems.Combat.Battle
 			
 		}
 
-		private static void ChangeCombatState(GameEntity battleEntity, Components.Combat.Battle.Battle.BattleState newState)
+		private void ChangeCombatState(GameEntity battleEntity, Components.Combat.Battle.Battle.BattleState newState)
 		{
 			Components.Combat.Battle.Battle battle = battleEntity.battle;
 			battle.State = newState;
 			battleEntity.ReplaceBattle(battle.Enemies, battle.CardDrawLevel, battle.State);
+
+			switch (newState)
+			{
+				case Components.Combat.Battle.Battle.BattleState.Draw:
+					ExecuteDraw(battleEntity);
+					break;
+				case Components.Combat.Battle.Battle.BattleState.EnemyTurn:
+					ExecuteEnemyTurn(battleEntity);
+					break;
+				case Components.Combat.Battle.Battle.BattleState.Play:
+					ExecutePlay();
+					break;
+			}
+			
+		}
+
+		private void ExecutePlay()
+		{
+			
+		}
+
+		private void ExecuteEnemyTurn(GameEntity battleEntity)
+		{
+			GameEntity result = _context.CreateEntity();
+			result.AddChangeCombatStateEvent(Components.Combat.Battle.Battle.BattleState.Draw);
+		}
+
+		private void ExecuteDraw(GameEntity battleEntity)
+		{
+			Components.Combat.Battle.Battle battle = battleEntity.battle;
+			_combatService.CreateDrawCardEvent(battle.CardDrawLevel);
+			GameEntity result = _context.CreateEntity();
+			result.AddChangeCombatStateEvent(Components.Combat.Battle.Battle.BattleState.Play);
 		}
 
 		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
