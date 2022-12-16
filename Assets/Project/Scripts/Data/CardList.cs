7b3d52d481b44d3c60d43938d07d51bb63668f8e
Miguel Barreiro <miguelb@gram.gs>
2022-12-16 21:51:04 +0000

    changed card lists to use the new format

--- Assets/Project/Scripts/Data/CardList.cs
+++ Assets/Project/Scripts/Data/CardList.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace MergeToStay.Data
@@ -11,5 +12,12 @@ namespace MergeToStay.Data
 		
 		[SerializeField]
 		private List<CardData> AllCards;
+		
+		[Button("Switch To Cards")]
+		private void SwitchToCards()
+		{
+			foreach (CardData cardData in AllCards)
+				Cards.Add(new CardsModel.Card() {Level = 0, CardData = cardData});
+		}
 	}
 }
